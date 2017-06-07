9f1f5309fab56f8885978ea781a0392fb8ce13f4
Susanna Huang <dearsusannahuang@gmail.com>
2017-06-06 21:37:08 -0400

    pagination added to photo view

--- src/Pagination.js
+++ src/Pagination.js
@@ -1,37 +1,37 @@
-import React, { Component } from 'react';
-import { Pagination } from 'react-bootstrap';
+// import React, { Component } from 'react';
+// import { Pagination } from 'react-bootstrap';
 
-//class PhotoView extends Component
-// const PaginationAdvanced = React.createClass({
-class PaginationTool extends Component {
-  constructor(){
-      super();
-      this.state = {
-          activePage: 1
-      }
-  };
+// //class PhotoView extends Component
+// // const PaginationAdvanced = React.createClass({
+// class PaginationTool extends Component {
+//   constructor(){
+//       super();
+//       this.state = {
+//           activePage: 1
+//       }
+//   };
 
-  handleSelect = (eventKey) => {
-    this.setState({
-      activePage: eventKey
-    });
-  };
+//   handleSelect = (eventKey) => {
+//     this.setState({
+//       activePage: eventKey
+//     });
+//   };
 
-  render() {
-    return (
-      <Pagination
-        prev
-        next
-        first
-        last
-        ellipsis
-        boundaryLinks
-        items={20}
-        maxButtons={5}
-        activePage={this.state.activePage}
-        onSelect={this.handleSelect} />
-    );
-  }
-};
+//   render() {
+//     return (
+//       <Pagination
+//         prev
+//         next
+//         first
+//         last
+//         ellipsis
+//         boundaryLinks
+//         items={20}
+//         maxButtons={5}
+//         activePage={this.state.activePage}
+//         onSelect={this.handleSelect} />
+//     );
+//   }
+// };
 
-export default PaginationTool;
+// export default PaginationTool;
